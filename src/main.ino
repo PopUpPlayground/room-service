ab9314c7458dc7be8579d90a65c4732f57cd6211
Paul Fenwick <pjf@perltraining.com.au>
2016-09-05 23:24:24 +1000

    LCD working!!

--- src/main.ino
+++ src/main.ino
@@ -1,11 +1,26 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// 0x27 - I2C bus address
+
+LiquidCrystal_I2C   lcd(0x27,2,1,0,4,5,6,7);
+
 const int Pwr_Led = 13;
 
 void setup() {
     // Turn on the power LED, to show we're running.
     pinMode(Pwr_Led, OUTPUT);
     digitalWrite(Pwr_Led, HIGH);
+
+    // activate LCD module
+    lcd.begin (16,2); // for 16 x 2 LCD module
+    lcd.setBacklightPin(3,POSITIVE);
+    lcd.setBacklight(HIGH);
 }
 
 void loop() {
-
+    lcd.home(); // set cursor to 0,0
+    lcd.print("Stay A While"); 
+    lcd.setCursor (0,1);        // go to start of 2nd line
+    lcd.print("Stay... FOREVER");
 }
