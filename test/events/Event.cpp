0afca3b79ffa823ed02f99dfd4f7167cffdba146
Paul Fenwick <pjf@perltraining.com.au>
2016-09-11 21:06:16 +1000

    Event queue testing. :)

--- test/events/Event.cpp
+++ test/events/Event.cpp
@@ -1,4 +1,5 @@
 #include "MsgEvent.h"
+#include "EventQueue.h"
 #include "unity.h"
 #include <string.h>
 
@@ -13,10 +14,43 @@ void test_MsgEvent() {
     TEST_ASSERT( strcmp( "bar", e.processEvent().c_str() ) );
 }
 
+// This would be better done with a closure and a variable we can
+// test.
+void helper_never_call_me(const char *) {
+    TEST_FAIL_MESSAGE("Never to be called function called");
+}
+
+void helper_pass_string(const char *input) {
+    TEST_ASSERT_EQUAL_STRING("pass", input);
+}
+
+void test_EventQueue_empty() {
+    EventQueue events;
+
+    // No events on the queue, so this should do nothing.
+    events.runEvents(helper_never_call_me, 9001);
+}
+
+void test_EventQueue_single() {
+    EventQueue events;
+
+    events.scheduleEvent(9000, new MsgEvent("pass") );
+
+    // Should not fire.
+    events.runEvents(helper_never_call_me,8999);
+
+    // Should fire
+    events.runEvents(helper_pass_string,9000);
+
+    // TODO: Test events queue is now empty.
+}
+
 int main(int argc, char **argv) {
     UNITY_BEGIN();
 
     RUN_TEST(test_MsgEvent);
+    RUN_TEST(test_EventQueue_empty);
+    RUN_TEST(test_EventQueue_single);
 
     UNITY_END();
 
