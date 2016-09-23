b0482c80615314344464bab82404da80720fdd06
Paul Fenwick <pjf@perltraining.com.au>
2016-09-23 13:39:29 +1000

    Goal picking behaviour exists.
    For #4

--- src/Goals.h
+++ src/Goals.h
@@ -2,7 +2,9 @@
 #define GOALS_H
 
 #include <vector>
+#include <stdlib.h>
 #include "types.h"
+#include <assert.h>
 
 // Struct that defines each goal.
 class Destination {
@@ -47,9 +49,60 @@ class Goals {
             totalWeightings += weight;
         }
 
+        // Given a current location, finds where this actor wants to go next.
+        const Destination* findGoal(const room_t currentRoom) {
+            const Destination* nextRoom;
+
+            // This just keeps rolling until we get a room that isn't ours.
+            while (nextRoom = pickRoom()) {
+                if (nextRoom->room != currentRoom) {
+                    return nextRoom;
+                }
+            }
+
+            // Should never, ever reach here.
+            assert(false);
+        }
+
     private:
         Destinations destinations;
-        unsigned int totalWeightings;
+        weight_t totalWeightings;
+
+        // Picks a room at random (using our weightings) and returns it.
+        // Pays no attention to the room we're currently in.
+        const Destination* pickRoom() {
+            // We create a random number in the range of 1 to totalWeightings
+            // (inclusive). We then walk through our table, adding each
+            // weighting to our accumulator as we go, it reaches or exceeds our
+            // roll at which point we've found our destination.
+            //
+            // We could do this by decrementing our roll, but we're using
+            // unsigned ints, and doing negative there would be bad.
+            //
+            // This is O(n), and I'm sure an O(1) solution exists, but meh,
+            // we've got heaps of CPU.
+            
+            // This has a small amount of skew, but the players won't notice so
+            // we don't care.
+            weight_t roll  = 1 + (rand() % totalWeightings);
+            weight_t accum = 0;
+
+            for (Destinations::const_iterator i = destinations.begin(); i != destinations.end(); ++i) {
+
+                // Increment our accumulator by the weight of the current room.
+                accum += (*i)->weight;
+
+                // If we've reached far enough in our table, return the element.
+                if (accum >= roll) {
+                    return *i;   // Returns a pointer to the entry we want.
+                }
+            }
+
+            // You know, we're never supposed to reach here. Something's gone very wrong.
+            
+            assert(false);
+        }
+
 
 };
 
--- test/goals/Goals.cpp
+++ test/goals/Goals.cpp
@@ -10,8 +10,23 @@ void test_Goal_simple() {
     goals.add(1, 50, 1000, 100);
     goals.add(3, 50, 1000, 100);
 
-    // Success!
-    TEST_ASSERT(true);
+    // With our simple table, we should always pick the room we're *not* in
+    // as our goal.
+    TEST_ASSERT_EQUAL(3, goals.findGoal(1)->room);
+    TEST_ASSERT_EQUAL(1, goals.findGoal(3)->room);
+
+    // Now add an extra room
+    goals.add(2, 50, 1000, 100);
+
+    // Walk through each room, picking a new destination for each,
+    // making sure it's not the current room. Do this 100 times, so
+    // if there's a weird edge case, we might find it.
+    for (int tests = 100; tests >= 0; tests--) {
+        for (int i=1; i <= 3; i++) {
+            TEST_ASSERT(i != goals.findGoal(i)->room);
+        }
+    }
+
 }
 
 int main(int argc, char **argv) {
