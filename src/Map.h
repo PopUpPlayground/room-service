ca66532040dd27742bce41cdd2afed3c2fd477ec
Paul Fenwick <pjf@perltraining.com.au>
2016-09-24 14:17:54 +1000

    Updated movement tests with new map class.
    The map structure is public again, whatever.

--- src/Map.h
+++ src/Map.h
@@ -11,11 +11,11 @@ typedef std::vector<path_t *> paths_t;
 
 class Map {
     private:
-        map_t map;
         void dumpVector(path_t *vector);
         void releasePaths(paths_t *paths);
 
     public:
+        map_t map;
         path_t *findPath(const room_t src, const room_t dst, const path_t *baseRoute);
         void newRoom(const char *name, const room_t number);
         void newBiDoor(const room_t r1, const room_t r2, const ident_t id);
--- test/movement/Movement.cpp
+++ test/movement/Movement.cpp
@@ -12,20 +12,17 @@ void consolePrint(const char *string) {
 
 // This tests a simple three-room map. The rooms are connected in a line.
 void test_Movement() {
-    map_t map;
+    Map map;
     Actor *cannibal;
 
-    map[1] = new Room("Foyer", 1);
-    map[1]->exits[2] = new DoorPortal(11);
+    map.newRoom("Foyer",1);
+    map.newRoom("Lounge",2);
+    map.newRoom("Bathroom",3);
 
-    map[2] = new Room("Lounge", 2);
-    map[2]->exits[1] = new DoorPortal(21);
-    map[2]->exits[3] = new DoorPortal(22);
+    map.newBiDoor(1,2,11);
+    map.newBiDoor(2,3,31);
 
-    map[3] = new Room("Bathroom", 3);
-    map[3]->exits[2] = new DoorPortal(31);
-
-    TEST_ASSERT_EQUAL_STRING(map[1]->name,"Foyer");
+    TEST_ASSERT_EQUAL_STRING(map.map[1]->name,"Foyer");
 
     // TODO: Test other properties.
 
@@ -35,24 +32,13 @@ void test_Movement() {
     TEST_ASSERT_EQUAL(1, cannibal->room);
 
     // MOVE THE CANNIBAL.
-    map[1]->exits[2]->trigger(
+    map.map[1]->exits[2]->trigger(
         consolePrint,
         cannibal,
-        map[2]
+        map.map[2]
     );
 
     TEST_ASSERT_EQUAL(2, cannibal->room);
-
-    // This frees up all the memory we allocated and makes
-    // valgrind happy. We should probably make a proper map class
-    // and give it a custom destructor.
-    for (map_t::iterator it=map.begin(); it!=map.end();) {
-        map_t::iterator old = it;
-        ++it;
-
-        delete old->second;
-        map.erase(old);
-    }
 
     delete cannibal;
 }
